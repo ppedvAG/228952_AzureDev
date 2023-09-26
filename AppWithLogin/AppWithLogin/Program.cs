8104d0620661183db8b0b3630c6b955ec37ae9cc
André <AndreR@ppedv.de>
2023-09-26 14:21:16 +0200

    Log mit App Insights

--- AppWithLogin/AppWithLogin/Program.cs
+++ AppWithLogin/AppWithLogin/Program.cs
@@ -1,12 +1,9 @@
 using AppWithLogin.Data;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Logging.ApplicationInsights;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
+using System.Data.Common;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,6 +23,16 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor()
     .AddMicrosoftIdentityConsentHandler();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Logging.AddApplicationInsights(
+        configureTelemetryConfiguration: (config) =>
+            config.ConnectionString = @"InstrumentationKey=0c123673-3570-4b6e-b6d6-8ba1a4419195;IngestionEndpoint=https://germanywestcentral-1.in.applicationinsights.azure.com/;LiveEndpoint=https://germanywestcentral.livediagnostics.monitor.azure.com/",
+            configureApplicationInsightsLoggerOptions: (options) => { }
+   ) ;
+
+//builder.Logging.AddFilter<ApplicationInsightsLoggerProvider>("AppWithLogin", LogLevel.Trace);
+
+
 
 var app = builder.Build();
 
